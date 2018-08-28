2e03100839ec23957c60ecc5e3863fbf71771c88
jchap136 <joechapman136@gmail.com>
2018-08-28 13:36:40 -0400

    exercise 4

---
+++ day2-lunch/day2-exercise-#4.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) > 1:
+    f = open(sys.argv[1])
+else:
+    f = sys.stdin
+
+list = []
+for i, line in enumerate(f):
+    if line.startswith("@"):
+        continue
+    cut = line.strip("\r\n").split("\t")
+    list.append(cut[2])
+    if len(list) > 10:
+        print(list)
+        break
+        
