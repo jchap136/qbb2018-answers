ce2542a6d713f1b144002d2510fb1b5380829568
jchap136 <joechapman136@gmail.com>
2018-08-29 14:33:32 -0400

    exercise 3 with comments

--- day3-lunch/day3-lunch-#3.py
+++ day3-lunch/day3-lunch-#3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# run this python command followed by 
+# run this python command followed by file
 
 import sys
 
@@ -9,7 +9,8 @@ if len(sys.argv) > 1:
 else:
     f = sys.stdin
     
-interest = 21378950
+interest = 21378950 # region of interest
+
 #protein coding
 closest_dist = 100000000000
 my_dist = 0
@@ -23,19 +24,20 @@ for line in f:
     fields = line.rstrip("\r\n").split()
     if line.startswith("#"):
         continue
-    if fields[2] == "gene" and fields[0] == "3R":
-        start = int(fields[3])
-        end = int(fields[4])
+    if fields[2] == "gene" and fields[0] == "3R": # only looking for genes in chromosome 3R
+        start = int(fields[3]) # start position
+        end = int(fields[4]) # end position
         
-        if "protein_coding" in line:
+        if "protein_coding" in line: # protein coding genes
             if start > interest:
-                my_dist = start - interest
+                my_dist = start - interest # my_dist is a number now
             elif end < interest:
-                my_dist = interest - end
+                my_dist = interest - end # my_dist is a number now
             if my_dist < closest_dist:
-                closest_dist = my_dist
-                closest_name = fields[13]
-        if "protein_coding" not in line:
+                closest_dist = my_dist # now we will override closest_dist with my_dist if lower
+                closest_name = fields[13] # list gene name is my_dist is lower
+                
+        if "protein_coding" not in line: # non-protien coding genes, same thing as above
             if start > interest:
                 my_dist_n = start - interest
             elif end < interest:
