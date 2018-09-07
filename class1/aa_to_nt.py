27b8d7d192fd2b45ffd0efa5aa5d92ef751f7147
jchap136 <joechapman136@gmail.com>
2018-09-07 19:41:24 -0400

    aa to nt script

---
+++ class1/aa_to_nt.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""
+Usage: ./aa_to_nt.py <seqs.fa> <aligned_aa.fa>
+
+convert aa to nt
+"""
+
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import fasta
+import itertools
+
+dna_reader = fasta.FASTAReader(open(sys.argv[1]))
+aa_reader = fasta.FASTAReader(open(sys.argv[2]))
+
+all_nt = []
+all_aa = []
+
+# this first for loop iterates through sequence by sequence
+for (dna_id, dna), (aa_id, aa) in zip(dna_reader, aa_reader): # zip parses 2 files at once
+    nuclist = []
+    aa_list = []
+    #print(aa)
+    j = 0
+    # this for loop iterates each aa within a sequence
+    for i in range(len(aa)):      
+        a = aa[i]
+        aa_list.append(a)
+        nt = dna[j*3:(j*3)+3]
+        if a == "-":
+            nuclist.append("---")
+        else:
+            nuclist.append(nt)
+            j += 1 # only increase when aa exists, otherwise lose nt
+    all_nt.append(nuclist) # append list into bigger list
+    all_aa.append(aa_list) # append list into bigger list
+
+# so each nuclist is a list of codons, and it gets appended to all_nt each time, so each list within all_nt list
+# is a different sequence
+print(all_nt)
+#print(all_aa)
