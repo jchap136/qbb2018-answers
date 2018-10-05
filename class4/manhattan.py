3b2d13eb5ac1daa6d12aec79b66cedab4ec413fc
jchap136 <joechapman136@gmail.com>
2018-10-05 13:30:59 -0400

    manhattan script

---
+++ class4/manhattan.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+"""
+Usage: ./manhattan.py *.qassoc
+
+make manhattan plots from .qassoc
+"""
+
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+from cycler import cycler
+
+
+for fname in sys.argv[1:]:
+    treatment = fname.split('.')[1]
+    #print(treatment)
+    with open(fname) as f:
+    #f = open(sys.argv[1])
+        chrom_list = []
+        bp_list = []
+        p_list = []
+        for i, line in enumerate(f):
+            if i == 0:
+                continue
+            fields = line.split() # file split by random amount of whitespace, this ignores amount of whitespace
+            chrom = fields[0]
+            bp = fields[2]
+            p = fields[-1]
+            if "NA" not in p:
+                p = np.log(float(p))
+                p = -p
+                chrom_list.append(chrom)
+                bp_list.append(int(bp))
+                p_list.append(p)
+        #print(len(chrom_list))
+        #print(bp_list[0])
+        #print(len(p_list))
+
+        # now we have 3 lists and want to iterate over them at the same time to make plot
+        # need to shift plot over and change colors every time 'chrom_list' changes
+        plt.rc('axes', prop_cycle=(cycler('color', ['r', 'g', 'b', 'y', 'm', 'c',\
+         'salmon', 'darkviolet', 'tomato', 'chocolate', 'aqua', \
+         'deeppink', 'lime', 'crimson', 'darkgreen', 'orchid'])))
+        fig, ax = plt.subplots()
+        curr_chr = 1
+        #biglist = [[[bp_list[0]]]] # collection of sublists
+        biglist = []
+        sublist = [[], []] # list of 2 lists - bp and p
+        offset = 0
+        for i in range(len(chrom_list)):
+            chrom = chrom_list[i]
+            bp = bp_list[i]
+            p = p_list[i]
+
+            if i == 0 or chrom == chrom_list[i-1]:
+                # add bp and p to sublist
+                sublist[0].append(bp) # x value  
+                sublist[1].append(p) # y value
+                # if using biglist from line 47 and appending sublist: [[[1074]], [[x1], [y1]], [[x2], [y2]]]
+            else:
+                # set tmp value, plot values in same chr, then set offset values for next chr
+                tmp = sublist[0][0] 
+                for i, val in enumerate(sublist[0]):
+                    sublist[0][i] = (val - tmp) + offset
+                    if i > len(sublist[0]):
+                        ax.set_xticklabels(str(curr_chr))
+                offset = sublist[0][-1] # add last value of previous chr to next chr series
+                plt.scatter(sublist[0], sublist[1], alpha=0.1, s=10) # then hopefully it changes color next time
+                #biglist.append(sublist) # add sublist to biglist
+                #print(sublist[0][0])
+                
+                
+                sublist = [[], []] # empty sublist to prep for next series of chr
+               
+                curr_chr += 1
+                sublist[0].append(bp)
+                sublist[1].append(p)
+
+        ax.axhline(y=5, color='black', linewidth=1, linestyle='--')
+        ax.set_title("{}".format(treatment))
+        ax.set_xlabel("chromosome")
+        ax.set_ylabel("-log(p)")
+        fig.savefig("test.png")
+        fig.savefig("manhattan_{}.png".format(treatment))
+        plt.close(fig)
+
+
+### fail, too slow ###
+        # for i, (chrom, bp, p) in enumerate(zip(chrom_list, bp_list, p_list)):
+        #     print(i)
+        #     if i == 0 or (bp < bp_list[i-1] and chrom != chrom_list[i-1]): # when chromosome changes
+        #         bp_new = bp_list[i-1] + 1
+        #         print(bp_new)
+        #         ax.scatter(bp_new, p, alpha=0.5)#use color changer here
+        #         #ax.set_xticklabels(chrom)
+        #     elif bp < bp_list[i-1]: # after chromosome change, bp values need to be adjusted
+        #         bp_new = bp_list[i-1] + bp
+        #         print(bp_new)
+        #         ax.scatter(bp_new, p, alpha=0.5)#use current color
+        #         #ax.set_xticklabels(chrom)
+        #     else: # this would be only for chr1
+        #         print(bp)
+        #         ax.scatter(bp, p, alpha=0.5, color="green")
+        #         #ax.set_xticklabels(chrom)
+
+
